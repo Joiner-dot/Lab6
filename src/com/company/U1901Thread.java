a3c94c82ede5d4d15d58152945a79b6d57270aea
George <egor_kuzmin22@mail.ru>
2020-03-18 01:02:49 +0300

    Lab6

--- src/com/company/U1901Thread.java
+++ src/com/company/U1901Thread.java
@@ -1,22 +1,22 @@
 package com.company;
 
-public class U1901Thread extends Thread {
+public class U1901Thread extends Thread{
 
-    U1901Thread (U1901Bank bank, int trans, long sleep) {
+    U1901Thread (U1901Bank bank, int trans, long sleep){
         this.bankword = bank;
         this.intTrans = trans;
         this.lngSleep = sleep;
     }
 
-    public U1901Bank getBankword () {
+    public U1901Bank getBankword (){
         return bankword;
     }
 
-    public int getIntTrans () {
+    public int getIntTrans (){
         return intTrans;
     }
 
-    public long getLngSleep () {
+    public long getLngSleep (){
         return lngSleep;
     }
 
@@ -24,7 +24,7 @@ public class U1901Thread extends Thread {
     private int intTrans;
     private long lngSleep;
 
-    public void run () {
+    public void run (){
         bankword.calc (intTrans, lngSleep);
     }
 
