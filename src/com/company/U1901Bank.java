7530a1bdd82eebe6d30274167c1e9c093fc4baf1
George <egor_kuzmin22@mail.ru>
2020-03-18 01:02:49 +0300

    Lab6

--- src/com/company/U1901Bank.java
+++ src/com/company/U1901Bank.java
@@ -1,15 +1,15 @@
 package com.company;
 
-public class U1901Bank {
+public class U1901Bank{
     int intTo, intFrom = 220;
 
-    public synchronized void calc (int transaction, long lngTimeout) {
+    public synchronized void calc (int transaction, long lngTimeout){
         System.out.println ("Current values = " + intTo + " " + intFrom + " " + Thread.currentThread ().getName ());
         intFrom = intFrom - transaction;
 
-        try {
+        try{
             Thread.sleep (lngTimeout);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException e){
             e.printStackTrace ();
         }
         intTo += transaction;
